1ab613e6fdc94b9f9264298eea992b1d65a8d58c
Danila Kulinkovich <81530683+kulick23@users.noreply.github.com>
2023-01-26 23:01:08 +0200

    Update Test.js

--- Test.js
+++ Test.js
@@ -182,3 +182,42 @@ get maxPassengers() {
     }
   }
 }
+const DATABASE = {
+    dealer: {
+        title: 'Trucks & Buses Vilnius LTD'
+    },
+    trucks: [
+        {
+            vin: 1112,
+            color: 'Red',
+            carryWeight: 10
+        },
+        {
+            vin: 2332,
+            color: 'Yellow',
+            carryWeight: 20
+        },
+        {
+            vin: 5234,
+            color: 'Green',
+            carryWeight: 70
+        }
+    ],
+    buses: [
+        {
+            vin: 1112,
+            color: 'Green',
+            maxPassengers: 50
+        },
+        {
+            vin: 6543,
+            color: 'Yellow',
+            maxPassengers: 25
+        }
+    ]
+}
+
+const dealer = new Dealer(DATABASE.dealer.title);
+const buses = DATABASE.buses.map(bus => {
+    return new Bus(bus.vin, bus.color, bus.maxPassengers); 
+});
